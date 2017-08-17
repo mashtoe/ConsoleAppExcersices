19bc787171a427ba64db9c71bc9a72e837ac6444
mashtoe <mashtoe@hotmail.com>
2017-08-17 11:35:05 +0200

    fix

--- GUI/Model.cs
+++ GUI/Model.cs
@@ -12,5 +12,24 @@ namespace ConsoleAppExcersices.GUI
         {
             videos = new List<Video>();
         }
+
+        public int Menu(List<MenuItem> items, string nameOfMenu)
+        {
+            Console.Clear();
+            Console.WriteLine($"------{nameOfMenu}------\n");
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                Console.WriteLine((i + 1) + ": " + items[i].Name);
+            }
+
+            int chosenItem;
+            while ((!int.TryParse(Console.ReadLine(), out chosenItem)) || chosenItem <= 0 || chosenItem > items.Count)
+            {
+                Console.WriteLine($"Enter number between 1-{items.Count}");
+            }
+            return chosenItem - 1;
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -24,14 +24,14 @@ namespace ConsoleAppExcersices
             menuCreate.MOdel = model;
             items.Add(menuCreate);
 
-            MenuCreate menuCreate = new MenuCreate("Create");
-            menuCreate.MOdel = model;
-            items.Add(menuCreate);
+            //MenuCreate menuCreate = new MenuCreate("Create");
+            //menuCreate.MOdel = model;
+            //items.Add(menuCreate);
 
 
             while (true)
             {
-                int chosenItem = Menu(items, "Main");
+                int chosenItem = model.Menu(items, "Main");
 
                 items[chosenItem].Action();
                 //Console.WriteLine(chosenItem);
@@ -42,24 +42,7 @@ namespace ConsoleAppExcersices
 
         }
 
-        private int Menu(List<MenuItem> items, string nameOfMenu)
-        {
-            Console.Clear();
-            Console.WriteLine($"------{nameOfMenu}------\n");
-            
-            for (int i = 0; i < items.Count; i++)
-            {
-                Console.WriteLine((i + 1) + ": " + items[i].Name);
-            }
-
-            int chosenItem;
-            while ((!int.TryParse(Console.ReadLine(),out chosenItem)) || chosenItem <= 0 || chosenItem > items.Count)
-            {
-                Console.WriteLine($"Enter number between 1-{items.Count}");
-            }
-            return chosenItem-1;
-            
-        }
+        
 
     }
 }

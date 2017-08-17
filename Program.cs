9ee0b4c7cdea05ff7c021fc4d76c035b6b0e0c56
mashtoe <mashtoe@hotmail.com>
2017-08-17 09:11:17 +0200

    1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppExcersices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
